3076c363e18bd6dd24d8999d2bfed71a70888b7b
apshiyas <apshiyas@gmail.com>
2021-01-08 17:39:10 -0600

    Update Logo.js

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -14,17 +14,19 @@ const Logo = ({
   );
 
   return (
+     
     <div
       {...props}
       className={classes}
     >
-      <h1 className="m-0">
+     <h1 className="m-0">
         <Link to="/">
           <Image
-            src={require('./../../../assets/images/logo.svg')}
+            //src={require('./../../../assets/images/logo.svg')}
+    src={require('./../../../assets/images/Logo_200x200.png')}
             alt="Open"
-            width={32}
-            height={32} />
+            width={100}
+            height={100} />
         </Link>
       </h1>
     </div>
